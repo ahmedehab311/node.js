96051302863c12bdd2eed21c4341e69770ceae58
ahmedehab311 <ahmedehab.aa47@gmail.com>
2025-02-10 11:40:20 +0200

    up

--- index.js
+++ index.js
@@ -119,15 +119,15 @@
 // app.listen(3001, () => {
 //   console.log("iam listing in port 3001");
 // });
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("home Page");
-  }
-  if (req.url === "/about") {
-    res.end("about Page");
-  }
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./index.html"));
 });
-server.listen(5000, () => {
-  console.log("server listening on port 5000 ...");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
