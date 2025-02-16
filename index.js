f331c7fe4fa746e7b4e629f97b62b790911c084d
ahmedehab311 <ahmedehab.aa47@gmail.com>
2025-02-17 01:32:24 +0200

    update

--- index.js
+++ index.js
@@ -119,15 +119,65 @@
 // app.listen(3001, () => {
 //   console.log("iam listing in port 3001");
 // });
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+
+// app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./index.html"));
+// });
+// app.listen(5000, () => {
+//   console.log("Server is running on port 5000");
+// });
+
 const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static("./public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./index.html"));
+app.get("^/$|/index(.html)?", (req, res) => {
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+  // res.redirect(301, "/new-page.html");
+});
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
+
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("hello load ");
+    next();
+  },
+  (req, res) => {
+    res.send("hello wrld");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+const three = (req, res, next) => {
+  console.log("three");
+  res.send("finshed");
+};
+app.get("/chain(.html)?", [one, two, three]);
+
+
+app.get("*/", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("server is running in 5000");
 });
